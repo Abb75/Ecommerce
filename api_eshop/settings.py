1f828e43361df0300efcb2d67d6ffa9020296a00
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-02-06 17:58:08 +0100

    update

--- api_eshop/settings.py
+++ api_eshop/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'product', 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
