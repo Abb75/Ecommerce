1583e781f74e633df8562327b70c331cbbef4e56
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-02-06 13:10:10 +0100

    update

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import views
 from rest_framework.routers import DefaultRouter
-from .views import ListProductViewSet
+from .views import ListProductViewSet, ListImagesProductViewSet
 
 
 router = DefaultRouter()
 router.register(r'product', ListProductViewSet, 'product')
+router.register(r'product/images', ListImagesProductViewSet, 'product-images')
 
 app_name = 'product'
 
