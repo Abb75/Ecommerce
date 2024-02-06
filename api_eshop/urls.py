119ec84a3e56ba483e2933759b7734397ce1d0d7
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-02-06 17:25:24 +0100

    update

--- api_eshop/urls.py
+++ api_eshop/urls.py
@@ -35,8 +35,3 @@ urlpatterns = [
 
 ]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
